7979e0251b60354f23a73e6f7cf38bbdf09d6717
edvin.hakobyan <edvin.hakobyan@F4SC-02195>
2019-09-20 11:06:09 +0400

    www

--- Sending/Sending.cs
+++ Sending/Sending.cs
@@ -12,29 +12,33 @@ namespace Sending
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "hello",
-                                     durable: false,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
 
-
-                for (int i = 0; i < int.MaxValue; i++)
+                using (var channel = connection.CreateModel())
                 {
-                    var message = $"{Path.GetRandomFileName()} {i}";
-                    var body = Encoding.Unicode.GetBytes(message);
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
 
-                    channel.BasicPublish(exchange: "",
-                                         routingKey: "hello",
-                                         basicProperties: null,
-                                         body: body);
 
-                    Console.WriteLine($"Sent {message}");
-                    Thread.Sleep(1000);
+                    for (int i = 0; i < int.MaxValue; i++)
+                    {
+                        var message = $"{Path.GetRandomFileName()} {i}";
+                        var body = Encoding.Unicode.GetBytes(message);
+
+                        channel.BasicPublish(exchange: "",
+                                             routingKey: "hello",
+                                             basicProperties: null,
+                                             body: body);
+
+                        Console.WriteLine($"Sent {message}");
+                        Thread.Sleep(1000);
+                    }
                 }
             }
+        
 
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
