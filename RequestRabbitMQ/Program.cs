959890086e80cfbf4dad5920d19e89887d116266
edvin.hakobyan <edvin.hakobyan@F4SC-02195>
2019-09-23 12:51:50 +0400

    www

---
+++ RequestRabbitMQ/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RequestRabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserName = "guest";
+            string Password = "guest";
+            string HostName = "localhost";
+
+            var connectionFactory = new ConnectionFactory()
+            {
+                UserName = UserName,
+                Password = Password,
+                HostName = HostName
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+
+            //Creating Exchange
+            model.ExchangeDeclare("demoExchange", ExchangeType.Direct, true, true);
+            Console.WriteLine("Creating Exchange");
+
+            //Creating Queue
+            model.QueueDeclare("demoQueue", true, false, false, null);
+            Console.WriteLine("Creating Queue");
+
+            //Creating Binding
+            model.QueueBind("demoQueue", "demoExchange", "directexchange_key");
+            Console.WriteLine("Creating Binding");
+
+
+            var properties = model.CreateBasicProperties();
+            properties.Persistent = false;
+
+            var message = "Direct Message";
+            var messagebuffer = Encoding.Unicode.GetBytes(message);
+
+            model.BasicPublish("demoExchange", "directexchange_key", properties, messagebuffer);
+            Console.WriteLine("Message Send");
+
+
+            Console.ReadLine();
+        }
+    }
+}
